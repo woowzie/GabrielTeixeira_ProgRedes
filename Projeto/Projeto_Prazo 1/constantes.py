9080787b34725f08c1415964259dde72e2699929
Gabriel Teixeira <134982765+woowzie@users.noreply.github.com>
2023-12-03 23:49:49 -0300

    Create constantes.py

---
+++ Projeto/Projeto_Prazo 1/constantes.py
@@ -0,0 +1,5 @@
+HOST_SERVER = 'localhost'   # Definindo o IP do servidor
+SOCKET_PORT = 42500         # Definindo a porta
+BUFFER_SIZE = 512           # Definindo o tamanho do buffer
+CODE_PAGE   = 'utf-8'       # Definindo a página de códigos de caracteres-
+MAX_LISTEN  = 1             # Definindo o máximo de conexões enfileiradas 
