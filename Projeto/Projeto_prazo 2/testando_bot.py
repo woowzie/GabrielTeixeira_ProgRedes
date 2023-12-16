7848c24083e9624324345ce0931a45f60cb384a1
Gabriel Teixeira <134982765+woowzie@users.noreply.github.com>
2023-12-15 22:44:44 -0300

    Delete Projeto/Projeto_prazo 2/testando_bot.py
    tava usando pra testar os bots q criei

--- Projeto/Projeto_prazo 2/testando_bot.py
+++
@@ -1,8 +0,0 @@
-import requests, time
-
-while True:
-    token = '6687576480:AAFF7zx-2CYzRV77LB_0vjhMy9C2I5wVCBA'
-    strURL = f"https://api.telegram.org/bot{token}/getUpdates"
-    resultado = requests.get(strURL)
-    print(resultado.json())
-    time.sleep(15)
