71a92e261399ccffad404ec2c3dcd194a9e6a438
Gabriel Teixeira <134982765+woowzie@users.noreply.github.com>
2023-12-31 16:22:28 -0300

    Create constantes.py

---
+++ Projeto/Projeto_Prazo 3/constantes.py
@@ -0,0 +1,8 @@
+# criando uma página de constantes para facilitar a visualização das variáveis
+
+HOST_SERVER = 'localhost'
+PORT = 42500
+PROMPT = 'insira sua mensagem >> '
+SERVER = '0.0.0.0'
+CODE_PAGE = 'utf-8'
+
