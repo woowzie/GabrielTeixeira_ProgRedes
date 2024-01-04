300f5b46a7297b72aae611e3b9a9199b56916d68
Gabriel Teixeira <134982765+woowzie@users.noreply.github.com>
2024-01-03 23:54:03 -0300

    Update server.py

--- Projeto/Projeto_Prazo 3/server.py
+++ Projeto/Projeto_Prazo 3/server.py
@@ -1,9 +1,13 @@
-import threading, socket
+import threading, socket, requests
 from constantes import *
+from defs import *
 
 clients = list()
 
 def main():
+    bot_thread = threading.Thread(target=bot)
+    bot_thread.start()
+
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
@@ -40,5 +44,20 @@ def broadcast(msg, client, addr):
                 client_item.send(msg.encode(CODE_PAGE))
             except:
                 del_client(client_item)
+
+def bot():
+    update_id = None
+    token = '6829962164:AAEQm4YUovDIkWNwPUgJaZ-EMeNAe55w0vA'
+    url_base = f'https://api.telegram.org/bot{token}/'
+
+    while True:
+        atualizacao = obtendo_mensagens(update_id, url_base)
+        mensagens = atualizacao
+        if mensagens:
+            for mensagem in mensagens:
+                update_id = mensagem.get('update_id', update_id)
+                chat_id = mensagem['message']['from']['id']
+                resposta = verificar(mensagem, chat_id, url_base)
+                respondendo(resposta, chat_id, url_base)
 
 main()
